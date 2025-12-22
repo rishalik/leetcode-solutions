66a1d788065192cfff662c6e93d9e187abf8e38b
rishalik <41278501+rishalik@users.noreply.github.com>
2025-12-22 12:54:06 +0530

    Time: 216 ms (91.63%) | Memory: 17.2 MB (99.91%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -3,22 +3,23 @@ class Solution:
         if not s:
             return ""
         
+        start = 0
+        end = 0
         n = len(s)
-        st = 0
-        e = 0
-        
-        def expand(l, r):
-            while l >= 0 and r < n and s[l] == s[r]:
-                l -= 1
-                r += 1
-            return l+1, r-1
+
+        def expand_around_center(left, right):
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left+1, right-1
         
         for i in range(n):
-            l1, r1 = expand(i,i)
-            if r1-l1 > e-st:
-                st, e = l1, r1
+            l1, r1 = expand_around_center(i,i)
+            if r1 - l1 > end - start:
+                start, end = l1, r1
             
-            l2, r2 = expand(i, i+1)
-            if r2-l2 > e-st:
-                st, e = l2, r2
-        return s[st: e+1]
+            l2, r2 = expand_around_center(i,i+1)
+            if r2 - l2 > end - start:
+                start, end = l2, r2
+            
+        return s[start: end+1]
